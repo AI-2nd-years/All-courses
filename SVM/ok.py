dbefe8c46c30ffa2e3422652e741e26db1b68b2f
unknown <MMalam.mj@ilimi.edu.ne>
2025-04-07 14:56:49 +0100

    ajout du cours de SVM

---
+++ SVM/ok.py
@@ -0,0 +1,12 @@
+import requests
+
+url = "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-ML0101EN-SkillsNetwork/labs/Module%203/data/cell_samples.csv"
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    with open("data.csv","w") as file:
+        file.write(response.text)
+        print("success")
+else:
+    print("error")
